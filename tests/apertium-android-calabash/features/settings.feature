de116cefe73ae67d6df76abaa41434acde5178ba
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2013-03-13 19:47:50 +0000

    add test script
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-mobile/apertium-android@43068 72bbbca6-d526-0410-a7d9-f06f51895060

--- tests/apertium-android-calabash/features/settings.feature
+++ tests/apertium-android-calabash/features/settings.feature
@@ -2,4 +2,4 @@ Feature: Settings feature
 
   Scenario: As a valid user I can log into my app
     When I press "Settings"
-    Then I see "Mark the unknown words"
+    Then I see "unknown words"
---
+++ tests/run_calabash_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Calabash test scripts
+# see https://github.com/calabash/calabash-android
+#
+
+cd apertium-android-calabash
+
+
+calabash-android run ../../bin/apertium-android-debug.apk
+
